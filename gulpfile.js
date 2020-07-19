4353cc524c0d99adc8bd35d2389b28210047db6d
kunmishra2599 <kunmishra2599@gmail.com>
2020-07-19 22:36:57 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream());
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            index: "/index.html"
+        }
+    });
+    gulp.watch('app/scss/**/*.scss', style)
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
